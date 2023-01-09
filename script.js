4c03bca7b0a309dcdb164fe0e33b21749a367081
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2023-01-09 14:48:52 -0600

    animation

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Typeing Text
+function typeText() {
+    const textCont = document.getElementsByClassName("reveal-typeText");
+    for (let i = 0; i < textCont.length; i++) {
+        let text = textCont[i].textContent;
+        textCont[i].textContent = "";
+        for (let y = 0; y < text.length; y++) {
+            setTimeout(() => {
+                textCont[i].textContent += text[y];
+            }, y * 10);
+        }
+    }
+
+}
+
+
 //check if an element is in viewPort Function
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
@@ -8,34 +24,52 @@ function isInViewport(element) {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
-
+//check if class is already revealed
+function isActive(element) {
+    const classList = element.classList;
+    return classList.contains('active');
+}
 
 //add event Lister for scrolling
 window.addEventListener("scroll", reveal);
-function reveal(){
-    //for container that are not window
+let loadBarCalled = false;
+function reveal() {
     const allContainer = document.getElementsByClassName("reveal");
-    for(let i = 0; i < allContainer.length; i++){
-        if(!isInViewport(allContainer[i])){
+    for (let i = 0; i < allContainer.length; i++) {
+        //for container that are not visible
+        if (!isInViewport(allContainer[i]) && !isActive(allContainer[i])) {
             allContainer[i].classList.add("hidden");
         }
-        else{
+        else {
             allContainer[i].classList.remove("hidden");
+            allContainer[i].classList.add("active");
+            //for Loading Bar
+            if ((allContainer[i].getAttribute('id') === "chart-container") && (!loadBarCalled)) {
+                loadBar();
+                loadBarCalled = true;
+            }
+        }
+
+    }
+
+}
+//load language Bar
+function loadBar() {
+    const allBar = document.getElementsByClassName("chart-cont");
+    for (let i = 0; i < allBar.length; i++) {
+        //get widht length
+        let temp = allBar[i].getAttribute("id");
+        //console.log(temp);
+        var wit = document.getElementById(temp).clientWidth;
+        //console.log(wit);
+        //put width to 0
+       allBar[i].style.width = 0;
+        //load width back
+        for (let y = 0; y <= wit; y++) {
+            setTimeout(() => {allBar[i].style.width = String(y) + "px";}, y * 3);
         }
     }
+
 }
 
-// Typeing Text
-function typeText(){
-    const textCont = document.getElementsByClassName("reveal-typeText");
-    for(let i = 0; i < textCont.length; i++){
-        let text = textCont[i].textContent;
-        textCont[i].textContent = "";
-        for(let y = 0; y < text.length; y++){
-            setTimeout( () => {
-                textCont[i].textContent += text[y];
-            }, y * 10);
-        }
-    }
-    
-}
+
